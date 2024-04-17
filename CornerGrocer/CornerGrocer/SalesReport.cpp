57364095a3424ad3d44dc8365a6650b17609eb86
Greg Anderson <glanderson5@gmail.com>
2024-04-17 16:39:12 -0500

    Update SalesReport.cpp

--- CornerGrocer/CornerGrocer/SalesReport.cpp
+++ CornerGrocer/CornerGrocer/SalesReport.cpp
@@ -1,5 +1,21 @@
 #include "SalesReport.h"
+#include <iostream>
 
-void SalesReport::AddItem(ProduceItem item)
+void SalesReport::AddItem(ProduceItem& item)
 {
+	for (int i = 0; i < this->totalSales.size(); i++) {
+		if (this->totalSales.at(i).GetItemName() == item.GetItemName()) {
+			this->totalSales.at(i).SetNumPurchased(item.GetNumPurchased() + 1);
+			return;
+		}
+	}
+	this->totalSales.push_back(item);
+}
+
+void SalesReport::PrintReport()
+{
+	for (int i = 0; i < this->totalSales.size(); i++) {
+		ProduceItem currItem = this->totalSales.at(i);
+		cout << currItem.GetItemName() << " " << currItem.GetNumPurchased() << endl;
+	}
 }
